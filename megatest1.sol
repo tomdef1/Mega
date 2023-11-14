8c8b74e15ff43ce858c36390cf0c2e69c71c65d4
Tom_DeFi <106840127+tomdef1@users.noreply.github.com>
2023-11-14 18:29:01 +0000

    Update megatest1.sol
    + variable for max wallet
    + function to retrieve accidentally sent er20 tokens
    - restriction of buy/sell in same block

--- megatest1.sol
+++ megatest1.sol
@@ -8,14 +8,11 @@ import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
 
 contract MEGALODON is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
-    // Maximum wallet holding, initially set to 6% of total supply
-    uint256 public maxWalletHolding = 100000000 * 10 ** decimals() / 100 * 6;
+    // Maximum wallet holding set to 6,000,000 tokens
+    uint256 public maxWalletHolding = 6000000;
 
     // Mapping to keep track of exemptions from the max wallet holding
     mapping(address => bool) private _isExemptFromMaxHolding;
-
-    // Mapping to track the last block for each address
-    mapping(address => uint) private _lastTransactionBlock;
 
     // Event for updating max wallet holding
     event MaxWalletHoldingUpdated(uint256 newMax);
@@ -32,16 +29,14 @@ contract MEGALODON is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
     }
 
     /**
-     * @dev Override the transfer function to include custom logic for max wallet holding and same block transaction restriction.
+     * @dev Override the transfer function to include custom logic for max wallet holding.
      */
     function transfer(address to, uint256 amount) public override returns (bool) {
-        require(_lastTransactionBlock[msg.sender] != block.number, "Cannot buy and sell in the same block");
         require(
             _isExemptFromMaxHolding[to] || balanceOf(to) + amount <= maxWalletHolding,
             "Recipient exceeds the max wallet holding"
         );
 
-        _lastTransactionBlock[msg.sender] = block.number;
         return super.transfer(to, amount);
     }
 
@@ -81,5 +76,15 @@ contract MEGALODON is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
     function updateMaxWalletHolding(uint256 newMax) external onlyOwner {
         maxWalletHolding = newMax;
         emit MaxWalletHoldingUpdated(newMax);
+    }
+
+    /**
+     * @dev Allows users to retrieve tokens that were accidentally sent to the contract.
+     * @param tokenAddress The address of the ERC20 token to retrieve.
+     * @param tokenAmount The amount of tokens to retrieve.
+     */
+    function retrieveAccidentallySentTokens(address tokenAddress, uint256 tokenAmount) external {
+        require(tokenAddress != address(this), "Cannot retrieve tokens of this contract");
+        require(ERC20(tokenAddress).transfer(msg.sender, tokenAmount), "Token transfer failed");
     }
 }
